f6349b434dc425c703f23437f444aa3f55f66108
alexdebelka <53542458+alexdebelka@users.noreply.github.com>
2022-12-06 03:11:45 +0200

    BMI 2.0

---
+++ day 3/3_2.py
@@ -0,0 +1,21 @@
+def main():
+    weight=float(input("What is yout weight?\n"))
+    height=float(input("What is your height?\n"))
+    return weight, height
+def calculaate_bmi(weight, height):
+    bmi= weight/(height*height)
+    return bmi
+x,y= main()
+z= calculaate_bmi(x,y)
+# calculaate_bmi(weight, height)
+if z<=18.5:
+    print("You are underweight")
+elif 18.5<z<=25:
+    print("You are in normal range")
+elif 25<z<=30:
+    print("You are overweight")
+elif 30<z:
+    print("You are obise")
+else:
+    print("Unrecognized data")
+print("Your bmi is: ", z)
