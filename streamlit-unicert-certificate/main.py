5eddd39e575ba6fe68e0079cd097f9164dd4ed48
alexdebelka <53542458+alexdebelka@users.noreply.github.com>
2025-01-19 16:15:51 +0100

    939 Update main.py

--- streamlit-unicert-certificate/main.py
+++ streamlit-unicert-certificate/main.py
@@ -126,9 +126,9 @@ def main():
     st.write(f"Starting certificate number: {start_number}")
     
     # Add a reset button to reset the certificate number to 101
-    if st.button("Reset Starting Certificate Number to 935"):
-        save_last_cert_number(935)
-        st.success("Starting Certificate number reset to 935")
+    if st.button("Reset Starting Certificate Number to 939"):
+        save_last_cert_number(939)
+        st.success("Starting Certificate number reset to 939")
 
     # Step 4: Generate certificates when the button is clicked
     if st.button("Generate Certificates"):
